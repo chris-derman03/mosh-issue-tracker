6650026d4de2b05b5871eb4753ac73f01d0e4d22
Christian DerManuelian <christian.dermanuelian@gmail.com>
2025-03-14 17:37:43 -0700

    Style issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { prisma } from "@/prisma/client";
 import { notFound } from "next/navigation";
+import StatusBadge from "@/app/components/StatusBadge";
 
 const IssueDetailPage = async ({
     params,
@@ -15,11 +16,13 @@ const IssueDetailPage = async ({
     if (!issue) notFound();
 
     return (
-        <div>
-            <p>{issue.title}</p>
-            <p>{issue.description}</p>
-            <p>{issue.status}</p>
+        <div className="flex flex-col items-center gap-2 mt-20">
+            <div className="mb-8 flex flex-col items-center">
+                <p className="THEMED-text1">{issue.title}</p>
+                <StatusBadge status={issue.status} />
+            </div>
             <p>{issue.createdAt.toDateString()}</p>
+            <p>{issue.description}</p>
         </div>
     );
 };
