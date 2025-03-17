1e5cb3925e5b0bc39d6b38fa5a1544a97a918771
Christian DerManuelian <christian.dermanuelian@gmail.com>
2025-03-17 14:40:53 -0700

    Handle deletion errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -9,59 +9,95 @@ const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
     const [isRootClicked, setRootClicked] = useState(false);
     const [isDialogClicked, setDialogClicked] = useState(false);
     const router = useRouter();
+    const [deletionError, setDeletionError] = useState(false);
 
     return (
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-                <Button
-                    disabled={isRootClicked}
-                    onClick={() => {
-                        setRootClicked(true);
-                        setDialogClicked(false);
-                    }}
-                    className="THEMED THEMED-button"
-                >
-                    <FaRegTrashAlt size={18} />
-                </Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content>
-                <AlertDialog.Title className="THEMED-text6">
-                    Confirm Deletion
-                </AlertDialog.Title>
-                <AlertDialog.Description>
-                    Do you really want to permanently delete this issue?
-                </AlertDialog.Description>
-                <div className="flex gap-3 mt-4">
-                    <AlertDialog.Cancel>
-                        <Button
-                            className="THEMED-button-cancel"
-                            disabled={isDialogClicked}
-                            onClick={() => {
-                                setRootClicked(false);
-                                setDialogClicked(true);
-                            }}
-                        >
-                            Cancel
-                        </Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button
-                            className="THEMED-button"
-                            disabled={isDialogClicked}
-                            onClick={async () => {
-                                setRootClicked(false);
-                                setDialogClicked(true);
-                                await axios.delete(`/api/issues/${issueId}`);
-                                router.push("/issues");
-                                router.refresh();
-                            }}
-                        >
-                            Delete
-                        </Button>
-                    </AlertDialog.Action>
-                </div>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
+        <>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button
+                        disabled={isRootClicked}
+                        onClick={() => {
+                            setRootClicked(true);
+                            setDialogClicked(false);
+                        }}
+                        className="THEMED THEMED-button"
+                    >
+                        <FaRegTrashAlt size={18} />
+                    </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content>
+                    <AlertDialog.Title className="THEMED-text6">
+                        Confirm Deletion
+                    </AlertDialog.Title>
+                    <AlertDialog.Description>
+                        Do you really want to permanently delete this issue?
+                    </AlertDialog.Description>
+                    <div className="flex gap-3 mt-4">
+                        <AlertDialog.Cancel>
+                            <Button
+                                className="THEMED-button-cancel"
+                                disabled={isDialogClicked}
+                                onClick={() => {
+                                    setRootClicked(false);
+                                    setDialogClicked(true);
+                                }}
+                            >
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button
+                                className="THEMED-button"
+                                disabled={isDialogClicked}
+                                onClick={async () => {
+                                    setRootClicked(false);
+                                    setDialogClicked(true);
+                                    try {
+                                        await axios.delete(
+                                            `/api/issues/${issueId}`
+                                        );
+                                        router.push("/issues");
+                                        router.refresh();
+                                    } catch (error) {
+                                        setDeletionError(true);
+                                        setDialogClicked(false);
+                                    }
+                                }}
+                            >
+                                Delete
+                            </Button>
+                        </AlertDialog.Action>
+                    </div>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+
+            <AlertDialog.Root open={deletionError}>
+                <AlertDialog.Content>
+                    <AlertDialog.Title className="THEMED-text6">
+                        Deletion Failed.
+                    </AlertDialog.Title>
+                    <AlertDialog.Description>
+                        This issue could not be deleted. Please try again later.
+                    </AlertDialog.Description>
+                    <div className="mt-4">
+                        <AlertDialog.Cancel>
+                            <Button
+                                className="THEMED-button-cancel"
+                                disabled={isDialogClicked}
+                                onClick={() => {
+                                    setRootClicked(false);
+                                    setDialogClicked(true);
+                                    setDeletionError(false);
+                                }}
+                            >
+                                OK
+                            </Button>
+                        </AlertDialog.Cancel>
+                    </div>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+        </>
     );
 };
 
