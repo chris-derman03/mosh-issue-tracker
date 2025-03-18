353b9624182df6e7dce7f8db3671aed2c85122a9
Christian DerManuelian <christian.dermanuelian@gmail.com>
2025-03-18 10:17:09 -0700

    Edit api/issues/[id] PATCH to be able to add user id

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,36 +1,51 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
+// Edit the fields of a given Issue
 export async function PATCH(
     request: NextRequest, 
     {params}: {params: Promise<{id: string}>}) 
 {
-    const session = await getServerSession(authOptions);
-    if (!session)
-        return NextResponse.json({error: "You are not an authenticated user."}, {status: 401});
+    // Only authenticated users can make patches
+    // const session = await getServerSession(authOptions);
+    // if (!session)
+    //     return NextResponse.json({error: "You are not an authenticated user."}, {status: 401});
 
-    const body = await request.json();
-    const { id } = await params;
+    const patchingIssueId = await params.then(res => res.id);
 
-    const zodValidation = issueSchema.safeParse(body);
+    // Does the issue exist
+    const issue = await prisma.issue.findUnique({
+        where: {id: patchingIssueId}
+    })
+    if (!issue)
+        return NextResponse.json({error: 'Issue not found.'}, {status: 404})
+
+    // Validate the request
+    const requestBody = await request.json();
+    const zodValidation = patchIssueSchema.safeParse(requestBody);
         if (!zodValidation.success)
             return NextResponse.json(zodValidation.error.format(), {status: 400})
 
-    const issue = await prisma.issue.findUnique({
-        where: {id: id}
-    })
+    const { title, description, assignedToUserId } = requestBody;
 
-    if (!issue)
-        return NextResponse.json({error: 'Invalid Issue'}, {status: 404})
+    // If assigning a user, check if that user exists
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({
+            where: { id: assignedToUserId }
+        })
+        if (!user)
+            return NextResponse.json({error: "User not found."}, {status: 400})
+    }
 
     const updatedIssue = await prisma.issue.update({
-        where: {id: id},
+        where: {id: patchingIssueId},
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId
         }
     })
 
